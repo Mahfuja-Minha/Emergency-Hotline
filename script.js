7b31d75147d51e9882aa410ad2b04f819b8d8788
minha <mahfujaakterminha@gmail.com>
2025-09-13 22:12:47 +0600

    complete call coin and history add button using js

--- script.js
+++ script.js
@@ -26,3 +26,51 @@ for(const copyBtn of copyButton){
   })
   
 }
+
+// call, coin & history add
+const callButton = document.querySelectorAll(".btn-call");
+const coin = document.getElementById("coin-number");
+const historyDiv = document.getElementById("history-container");
+
+for (const btn of callButton) {
+  btn.addEventListener("click", function (e) {
+    const card = e.target.closest(".card");
+    const serviceName = card.querySelector("h2").innerText;
+    const serviceNumber = card.querySelector(".phn-number").innerText;
+
+    let coins = parseInt(coin.innerText);
+
+    if (coins < 20) {
+      alert(
+        `Not enough coins to make a call!...You need at least 20 coins to proceed`
+      );
+    } 
+    else {
+      alert(`Calling ${serviceName} at ${serviceNumber}.....`);
+      coins -= 20;
+      coin.innerText = coins;
+
+      const time = new Date().toLocaleTimeString();
+
+      const div = document.createElement("div");
+      div.classList.add(
+        "flex",
+        "justify-between",
+        "p-3",
+        "mb-3",
+        "bg-[#F2F2F280]",
+        "rounded-md"
+      );
+      div.innerHTML = `
+         <div >
+            <p class='font-semibold'>${serviceName}</p>
+            <h3 class='font-semibold'>${serviceNumber}</h3>
+          </div>
+          <span class='font-semibold'>${time}</span>
+      `;
+
+      historyDiv.appendChild(div);
+    }
+  });
+}
+
